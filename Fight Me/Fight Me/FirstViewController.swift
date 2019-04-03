d219e1c923f45d48c0960ab14971139932c297c6
Lauren DiDonato <lad4284@rit.edu>
2019-04-03 15:14:24 -0400

    Xcode project created

---
+++ Fight Me/Fight Me/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Fight Me
+//
+//  Created by Lauren DiDonato on 4/3/19.
+//  Copyright © 2019 Lauren DiDonato. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
