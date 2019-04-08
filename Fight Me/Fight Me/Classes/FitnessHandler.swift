29a689ff7994ff69465cf45a5386d75284471630
Ariel <aviggiano836@gmail.com>
2019-04-08 16:00:51 -0400

    Added files and file structure

---
+++ Fight Me/Fight Me/Classes/FitnessHandler.swift
@@ -0,0 +1,13 @@
+//
+//  FitnessHandler.swift
+//  Fight Me
+//
+//  Created by Student on 4/8/19.
+//  Copyright © 2019 Lauren DiDonato. All rights reserved.
+//
+
+import UIKit
+
+class FitnessHandler: NSObject {
+
+}
