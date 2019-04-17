ccfaf23193e5b988ef486ce839aebf5ab7da4786
Ariel <aviggiano836@gmail.com>
2019-04-17 14:46:44 -0400

    Added tablevc functions to ShopController

---
+++ Fight Me/Fight Me/Controllers/EquipmentDetailController.swift
@@ -0,0 +1,30 @@
+//
+//  EquipmentDetailController.swift
+//  Fight Me
+//
+//  Created by Student on 4/17/19.
+//  Copyright © 2019 Ariel Viggiano. All rights reserved.
+//
+
+import UIKit
+
+class EquipmentDetailController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
