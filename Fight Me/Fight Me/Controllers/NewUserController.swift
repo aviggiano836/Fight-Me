9196e6138fc47fe066bd66ac98b1f61d5ee5ab12
Ariel <aviggino836@gmail.com>
2019-05-02 13:55:12 -0400

    Added validation for creating a new user

--- Fight Me/Fight Me/Controllers/NewUserController.swift
+++ Fight Me/Fight Me/Controllers/NewUserController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class NewUserController: UIViewController {
+class NewUserController: UIViewController, UITextFieldDelegate {
 
+    let maxIntChar = 2
+    let BAD_INPUT = UIColor(cgColor: #colorLiteral(red: 1, green: 0.6700618703, blue: 0.7056196374, alpha: 1))
+    let GOOD_INPUT = UIColor(cgColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0))
+    
     @IBOutlet weak var username: UITextField!
     @IBOutlet weak var birthdate: UIDatePicker!
     @IBOutlet weak var height_ft: UITextField!
@@ -19,12 +23,60 @@ class NewUserController: UIViewController {
     
     @IBAction func createUser(_ sender: Any) {
         //create user and send to landing page
-    
+        let inputs = validInputs()
+        if inputs.count == 5 {
+            //create user
+            
+        }
     }
     
+    /*
+     * Validate all the inputs and return them as a dictionary
+     */
+    func validInputs() -> [String:Any] {
+        var temp: [String:Any] = [:]
+        
+        temp = validateTextField(field: username, key: "username", text: username.text!, dict: temp)
+        temp["birthdate"] = birthdate.date //Date picker has default date
+        temp = validateTextField(field: height_ft, key: "height_ft", text: height_ft.text!, dict: temp)
+        temp = validateTextField(field: height_in, key: "height_in", text: height_in.text!, dict: temp)
+        temp = validateTextField(field: weight, key: "weight", text: weight.text!, dict: temp)
+        
+        return temp
+    }
+    
+    /*
+     * Validate that a text field has valid input string
+     *      if valid append the input to the given dictionary and return it
+     *          and change the text fields background color to white in case changed previously
+     *      if invalid change the background color or the given text field to BAD_INPUT, pink
+     */
+    func validateTextField(field: UITextField, key:String, text:String, dict: [String:Any]) -> [String:Any]{
+        var temp = dict
+        if text == "" {
+            field.backgroundColor = BAD_INPUT
+        } else {
+            temp[key] = text
+            field.backgroundColor = GOOD_INPUT
+        }
+        return temp
+    }
+    
+    //MARK - UITextField Delegates
+    // For height and weight textfields - must take in only numbers
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        //Input must be numbers,
+        let allowedCharacters = CharacterSet(charactersIn:"0123456789 ")//Has to be a number
+        let characterSet = CharacterSet(charactersIn: string)
+        return allowedCharacters.isSuperset(of: characterSet) && string.count < maxIntChar
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        height_ft.delegate = self
+        height_in.delegate = self
+        weight.delegate = self
 
         // Do any additional setup after loading the view.
     }
