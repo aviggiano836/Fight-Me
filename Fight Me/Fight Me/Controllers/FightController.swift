f7e0605167c48fa9c7b496472e06dbfd9c61ba40
ariel <aviggiano836@gmail.com>
2019-05-03 15:52:24 -0400

    Added bluetooth stuff from tutorial

--- Fight Me/Fight Me/Controllers/FightController.swift
+++ Fight Me/Fight Me/Controllers/FightController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import CoreBluetooth
 
 class FightController: UIViewController {
-
+    let SERVICE_UUID = CBUUID.init()
+    let WR_UUID = CBUUID.init()
+    let WR_PROPERTIES: CBCharacteristicProperties = .write
+    let WR_PERMISSIONS: CBAttributePermissions = .writeable
+    
+    var deviceUUID : UUID?
+    var deviceAttributes : String = ""
+    var selectedPeripheral : CBPeripheral?
+    var centralManager: CBCentralManager?
+    var peripheralManager = CBPeripheralManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        centralManager = CBCentralManager(delegate: self, queue: DispatchQueue.main)
+        peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
         // Do any additional setup after loading the view.
     }
     
@@ -28,3 +41,87 @@ class FightController: UIViewController {
     */
 
 }
+
+extension FightController : CBCentralManagerDelegate {
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        
+        if (central.state == .poweredOn){
+            
+            self.centralManager?.scanForPeripherals(withServices: [SERVICE_UUID], options: [CBCentralManagerScanOptionAllowDuplicatesKey : true])
+            
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        
+        if (peripheral.identifier == deviceUUID) {
+            
+            selectedPeripheral = peripheral
+        }
+    }
+    
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        
+        peripheral.delegate = self
+        peripheral.discoverServices(nil)
+        
+    }
+}
+
+extension FightController : CBPeripheralDelegate {
+    
+    func peripheral( _ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        
+        for service in peripheral.services! {
+            
+            peripheral.discoverCharacteristics(nil, for: service)
+        }
+    }
+    
+    func peripheral(
+        _ peripheral: CBPeripheral,
+        didDiscoverCharacteristicsFor service: CBService,
+        error: Error?) {
+        
+        for characteristic in service.characteristics! {
+            
+            let characteristic = characteristic as CBCharacteristic
+ 
+            
+        }
+    }
+}
+
+extension FightController : CBPeripheralManagerDelegate {
+    
+    func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
+        if (peripheral.state == .poweredOn) {
+            
+            let serialService = CBMutableService(type: SERVICE_UUID, primary: true)
+            let writeCharacteristics = CBMutableCharacteristic(type: WR_UUID,
+                                                               properties: WR_PROPERTIES, value: nil,
+                                                               permissions: WR_PERMISSIONS)
+            serialService.characteristics = [writeCharacteristics]
+            peripheralManager.add(serialService)
+                
+            let advertisementData = String(format: "Ready to Fight")
+            peripheralManager.startAdvertising([CBAdvertisementDataServiceUUIDsKey:[SERVICE_UUID],
+                                                CBAdvertisementDataLocalNameKey: advertisementData])
+        }
+    }
+    
+    func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveWrite requests: [CBATTRequest]) {
+        
+        for request in requests {
+            if let value = request.value {
+                
+                let messageText = String(data: value, encoding: String.Encoding.utf8) as String!
+                //appendMessageToChat(message: Message(text: messageText!, isSent: false))
+            }
+            self.peripheralManager.respond(to: request, withResult: .success)
+        }
+    }
+}
+
